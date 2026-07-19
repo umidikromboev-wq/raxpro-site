184df4bca1e548b48bbda673c346550572f7c8fe
abduraufrasulboyev <abduraufrasulboyev@gmail.com>
2026-07-19 22:03:11 +0500

    done

--- app/layout.jsx
+++ app/layout.jsx
@@ -47,6 +47,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru" className={`${manrope.variable} ${onest.variable}`}>
       <head>
+        {/* Google Tag Manager - Head Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W25KKCT7');`}
+        </Script>
+
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XZ0K3N301W"
           strategy="afterInteractive"
@@ -76,6 +85,16 @@ export default function RootLayout({ children }) {
         </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) - Body Element */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W25KKCT7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <SmoothScroll />
         <ScrollProgress />
         {children}
